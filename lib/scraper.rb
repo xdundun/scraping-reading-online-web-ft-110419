d725f14980401deac9ed8b818e7ec366cea07e24
Dardan Gjonbalaj <xdundun@gmail.com>
2019-12-02 04:48:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,3 +5,9 @@ html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(html)
 doc.css(".headline-26OIBN").text
+
+courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+
+courses.each do |course|
+  puts course.text.strip
+end
