b09396142f23b6008b7502fc2f4196af85e40f12
Dardan Gjonbalaj <xdundun@gmail.com>
2019-12-02 04:43:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,5 +3,5 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+doc = Nokogiri::HTML(html)
 doc.css(".headline-26OIBN").text
